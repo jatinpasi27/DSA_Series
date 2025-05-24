1492bd4ae6aa7df6ff36344bc09deb084b4c7398
Jatin Pasi <pasijatin2004@gmail.com>
2025-05-24 21:12:25 +0530

    Create Variables.cpp

---
+++ Variables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a = 23;
+float b = 32.433;
+cout<<"The variable a contains: " <<a<<endl;
+cout<<"The variable b contains: " <<b<<endl;
+return 0;
+}
